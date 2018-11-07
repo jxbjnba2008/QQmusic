32134ffc54ce9dd74bfb7344e7df07ac559b3330
jxbjnba2008 <jxbjnba2008@126.com>
2018-11-08 00:09:11 +0800

    Update qqmusic/pipelines.py

--- qqmusic/pipelines.py
+++ qqmusic/pipelines.py
@@ -4,8 +4,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from pymongo import MongoClient
 
 class QqmusicPipeline(object):
+    def __init__(self):
+        self.connect = MongoClient(host='localhost',port=27017)
+        self.spider = self.connect.qqmusic
+        self.col = self.spider.musicdata
     def process_item(self, item, spider):
+        self.col.insert({'singer':item['singer'],'song':item['song']})
         return item
