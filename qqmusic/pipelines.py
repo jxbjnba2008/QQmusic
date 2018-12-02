7762f491b0d2ffd3e3ec0eb6a9c56cf40fac7608
jxbjnba2008 <jxbjnba2008@126.com>
2018-12-02 15:49:11 +0800

    Update qqmusic/pipelines.py

--- qqmusic/pipelines.py
+++ qqmusic/pipelines.py
@@ -8,9 +8,12 @@ from pymongo import MongoClient
 
 class QqmusicPipeline(object):
     def __init__(self):
+        #连接mongodb
         self.connect = MongoClient(host='localhost',port=27017)
+        #建立数据库qqmusic
         self.spider = self.connect.qqmusic
-        self.col = self.spider.musicdata
+        #建立集合qqmusicdata
+        self.col = self.spider.qqmusicdata
     def process_item(self, item, spider):
-        self.col.insert({'singer':item['singer'],'song':item['song'],'albumname':item['albumname'],'time':item['time']})
+        self.col.insert({'singer':item['singer'],'song':item['song'],'albumname':item['albumname'],'time':item['time'],'download_url':item['download_url']})
         return item
